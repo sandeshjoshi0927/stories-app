7468a5ef07722bf59d2dad036b1a403303c91fdb
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2025-07-19 10:17:50 +0545

    implementing individual story page

--- frontend/src/pages/StoryPage.tsx
+++ frontend/src/pages/StoryPage.tsx
@@ -1,3 +1,23 @@
+import { Link, useParams } from "react-router-dom";
+import stories from "../data/content";
+
 export default function StoryPage() {
-  return "StoryPage";
+  const { name } = useParams();
+
+  const story = stories.find((story) => story.name === name)!;
+
+  return (
+    <>
+      <Link to="/stories">Go Back</Link>
+      <h1 className="text-2xl font-bold">{story.title}</h1>
+
+      <section className="flex flex-col gap-4 mt-4 lg:max-w-3xl lg:mx-auto">
+        {story.content.map((str) => (
+          <p key={str} className="text-gray-300">
+            {str}
+          </p>
+        ))}
+      </section>
+    </>
+  );
 }
